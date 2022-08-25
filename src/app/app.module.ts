0583238fdd372ecae0e38b6f5a950bce975636b2
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-08-25 16:41:10 -0600

    removed default html and practicing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { AnotherComponent } from './another/another.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    AnotherComponent
   ],
   imports: [
     BrowserModule,
