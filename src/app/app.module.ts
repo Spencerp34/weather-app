450440a42f94c870bdf8bdd93de85e340e09273b
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-08-29 14:57:59 -0600

    removed another folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
-import { AnotherComponent } from './another/another.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    AnotherComponent
   ],
   imports: [
     BrowserModule,
